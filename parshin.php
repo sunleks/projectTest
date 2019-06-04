c65f596d762a8c6336f8eadc7b25f9dec5f598a1
sunleks@users <67sasha1996@mail.ru>
2019-06-04 12:05:03 +0300

    БД

---
+++ parshin.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Export to PHP Array plugin for PHPMyAdmin
+ * @version 4.8.5
+ */
+
+/**
+ * Database `parshin`
+ */
+
+/* `parshin`.`cities` */
+$cities = array(
+  array('id' => '1','cities' => 'Москва'),
+  array('id' => '2','cities' => 'Смоленск'),
+  array('id' => '3','cities' => 'Велиж'),
+  array('id' => '4','cities' => 'Магадан')
+);
+
+/* `parshin`.`users` */
+$users = array(
+  array('name' => 'Андрей','age' => '56','id_country' => '2'),
+  array('name' => 'Алексей','age' => '25','id_country' => '4'),
+  array('name' => 'Оля','age' => '30','id_country' => '3'),
+  array('name' => 'Ира','age' => '53','id_country' => '1')
+);
